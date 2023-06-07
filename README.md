4020f454e13e16afcb81dad9857992ea9bf84a99
spmfox <spmfox@foxwd.com>
2023-06-07 00:29:18 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
-# EnigmaAnsible
+# AnsiblePilot
+
+Cockpit plugin to run Ansible playbooks inside Podman containers.
+
+![AnsiblePilot](docs/images/logo.png)
+
+This cockpit plugin gives the ability to run Ansible playbooks in (or not) a Podman container.
+The idea was to get some of the features of AWX but none of the bloat.
+
+## Installation
+- Use the included Ansible playbook to install: ```ansible-playbook install-plugin.yml```
+- OR - copy the ```AnsiblePilotPlugin``` directory to ```/usr/share/cockpit/AnsiblePilotPluguin```
+
+## Usage
+![Screenshot](docs/images/screenshot.png)
+
+## Requirements
+- Cockpit
+- Podman (optional if not using containers)
+- Ansible (optional if only running playbooks in containers)
+
+## Assumptions
+- Looks for .yml or .yaml files in ```/opt/playbooks```
+  - Gives the option to change this directory for the current run
+  - Can be changed permanently by changing the code
+- Looks for container images that have the label ```ansible_execution_environment```
+  - That means you have to build your own image and bake that label in
+  - This gives the flexibility to assign any mounts/ports/etc you need
+  - Sample containerfile, using AlmaLinux 9 and /opt/playbooks for a mount can be found in ```docs/ansible_execution_environment```
+- Gives the ability to add extra Ansible parameters at run-time with or without using containers
+
+## Usage Video
+![UsageVideo](docs/images/usage.gif)
+
+## Architecture
+This plugin was built using the [Cockpit Starter Kit](https://github.com/cockpit-project/starter-kit) project.
+To build your own, the files in ```src``` must be put into the corresponding folder within the starter-kit.
+The plugin itself then is generated using a ```make``` command in the starter-kit directory.
+More details on this process are on the starter-kit project page.
---
+++ src/app.jsx
@@ -0,0 +1,197 @@
+/*
+ * This file is part of Cockpit.
+ *
+ * Copyright (C) 2017 Red Hat, Inc.
+ *
+ * Cockpit is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * Cockpit is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Cockpit; If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import cockpit from 'cockpit';
+import React from 'react';
+import { Card, CardBody, CardTitle } from '@patternfly/react-core/dist/esm/components/Card/index.js';
+
+const _ = cockpit.gettext;
+
+export class Application extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      playbookFolder: '/opt/playbooks',
+      imageList: [],
+    };
+  }
+
+  componentDidMount() {
+    const populatePlaybookList = () => {
+      const playbookDropdown = document.getElementById('playbook');
+      const command = `ls ${this.state.playbookFolder} |grep -e .yml -e .yaml`;
+
+      const process = cockpit.spawn(['bash', '-c', command], { superuser: 'require', err: 'message' });
+
+      process.done((data) => {
+        const playbooks = data.split('\n');
+
+        playbookDropdown.innerHTML = '';
+
+        playbooks.forEach(function (playbook) {
+          if (playbook !== '') {
+            const option = document.createElement('option');
+            option.value = playbook;
+            option.textContent = playbook;
+            playbookDropdown.appendChild(option);
+          }
+        });
+      });
+
+      process.fail(function (error) {
+        console.error('Error fetching playbooks:', error);
+      });
+    };
+
+    const populateImageList = () => {
+      const imageNameDropdown = document.getElementById('image-name');
+      const command = 'podman image list --filter label=ansible_execution_environment --noheading --format "table {{.Repository}}"';
+
+      const process = cockpit.spawn(['bash', '-c', command], { superuser: 'require', err: 'message' });
+
+      process.done((data) => {
+        const images = data.split('\n');
+
+        this.setState({ imageList: images.filter((image) => image !== '') });
+
+        imageNameDropdown.innerHTML = '';
+
+        images.forEach(function (image) {
+          if (image !== '') {
+            const option = document.createElement('option');
+            option.value = image;
+            option.textContent = image;
+            imageNameDropdown.appendChild(option);
+          }
+        });
+      });
+
+      process.fail(function (error) {
+        console.error('Error fetching images:', error);
+      });
+    };
+
+    const executePlaybook = (playbook, extraArgs) => {
+      const runInContainer = document.getElementById('run-in-container').checked;
+      const imageName = document.getElementById('image-name').value;
+      const playbookFolder = this.state.playbookFolder;
+
+      let command = '';
+
+      if (runInContainer) {
+        command = `podman container runlabel ansible_execution_environment ${imageName} "${playbookFolder}/${playbook} ${extraArgs}"`;
+      } else {
+        command = `systemd-run --quiet --scope ansible-playbook ${playbookFolder}/${playbook} ${extraArgs}`;
+      }
+
+      const outputElement = document.getElementById('output');
+      outputElement.innerHTML = '';
+
+      const process = cockpit.spawn(['bash', '-c', command], { superuser: 'require', err: 'message' });
+
+      process.done(function () {
+        console.log('Playbook execution complete');
+      });
+
+      process.fail(function (error) {
+        console.error('Error executing playbook:', error);
+        outputElement.textContent += 'Error executing playbook: ' + error;
+      });
+
+      process.stream(function (data) {
+        outputElement.innerHTML += data;
+      });
+    };
+
+    document.getElementById('ansible-form').addEventListener('submit', function (event) {
+      event.preventDefault();
+
+      const selectedPlaybook = document.getElementById('playbook').value;
+      const extraArgs = document.getElementById('extra-args').value;
+
+      if (selectedPlaybook !== '') {
+        executePlaybook(selectedPlaybook, extraArgs);
+      }
+    });
+
+    document.getElementById('load-playbooks-btn').addEventListener('click', () => {
+      const newPlaybookFolder = document.getElementById('playbook-folder').value;
+      this.setState({ playbookFolder: newPlaybookFolder }, () => {
+        populatePlaybookList();
+      });
+    });
+
+    populatePlaybookList();
+    populateImageList();
+  }
+
+  render() {
+    return (
+      <div>
+        <Card>
+          <CardTitle>Run Ansible Playbook</CardTitle>
+          <CardBody>
+            <form id="ansible-form">
+              <div className="form-group">
+                <label htmlFor="playbook-folder">Playbook Folder:</label>
+                <input type="text" id="playbook-folder" className="form-control" placeholder="/opt/playbooks" />
+              </div>
+              <div className="form-group">
+                <label htmlFor="playbook">Select Playbook:</label>
+                <select id="playbook" className="form-control">
+                  <option value="">Choose a playbook</option>
+                </select>
+              </div>
+              <div className="form-group">
+                <label htmlFor="extra-args">Extra Arguments:</label>
+                <input type="text" id="extra-args" className="form-control" placeholder="Enter extra arguments" />
+              </div>
+              <div className="form-group">
+                <label htmlFor="image-name">Container Image:</label>
+                <select id="image-name" className="form-control">
+                  {this.state.imageList.map((image, index) => (
+                    <option key={index} value={image}>
+                      {image}
+                    </option>
+                  ))}
+                </select>
+              </div>
+              <div className="form-group">
+                <label htmlFor="run-in-container">Run in Container:</label>
+                <input type="checkbox" id="run-in-container" className="form-control" />
+              </div>
+              <button type="button" className="btn btn-primary" id="load-playbooks-btn">
+                Load Playbooks
+              </button>
+              <button type="submit" className="btn btn-primary">
+                Run Playbook
+              </button>
+            </form>
+          </CardBody>
+        </Card>
+        <Card>
+          <CardTitle>Output</CardTitle>
+          <CardBody>
+            <pre id="output"></pre>
+          </CardBody>
+        </Card>
+      </div>
+    );
+  }
+}
